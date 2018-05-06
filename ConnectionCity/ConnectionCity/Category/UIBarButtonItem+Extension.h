c3824fe4056612262a1c38399a9d5b6564891e37
孤独的流星 <1173918654@qq.com>
2018-05-06 23:41:13 +0800

    首页item带角标

--- ConnectionCity/ConnectionCity/Category/UIBarButtonItem+Extension.h
+++ ConnectionCity/ConnectionCity/Category/UIBarButtonItem+Extension.h
@@ -9,6 +9,27 @@
 #import <UIKit/UIKit.h>
 
 @interface UIBarButtonItem (Extension)
-+(UIBarButtonItem *)itemWithTarget:(id)target action:(SEL)action image:(NSString *)image title:(NSString *)title;
+@property (strong, nonatomic) UILabel *badge;
 
+// Badge value to be display
+@property (nonatomic) NSString *badgeValue;
+// Badge background color
+@property (nonatomic) UIColor *badgeBGColor;
+// Badge text color
+@property (nonatomic) UIColor *badgeTextColor;
+// Badge font
+@property (nonatomic) UIFont *badgeFont;
+// Padding value for the badge
+@property (nonatomic) CGFloat badgePadding;
+// Minimum size badge to small
+@property (nonatomic) CGFloat badgeMinSize;
+// Values for offseting the badge over the BarButtonItem you picked
+@property (nonatomic) CGFloat badgeOriginX;
+@property (nonatomic) CGFloat badgeOriginY;
+// In case of numbers, remove the badge when reaching zero
+@property BOOL shouldHideBadgeAtZero;
+// Badge has a bounce animation when value changes
+@property BOOL shouldAnimateBadge;
+
++(UIBarButtonItem *)itemWithTarget:(id)target action:(SEL)action image:(NSString *)image title:(NSString *)title EdgeInsets:(UIEdgeInsets )EdgeInsets;
 @end
--- ConnectionCity/ConnectionCity/Classes/Message/MessageController.m
+++ ConnectionCity/ConnectionCity/Classes/Message/MessageController.m
@@ -9,6 +9,9 @@
 #import "MessageController.h"
 
 @interface MessageController ()
+@property (strong,nonatomic)UIButton * tmpBtn;
+@property (weak, nonatomic) IBOutlet UIView *view_line;
+@property (weak, nonatomic) IBOutlet UIButton *btn_findPeople;
 
 @end
 
@@ -17,7 +20,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationController.navigationBar.barTintColor = [UIColor whiteColor];
+    [self setUI];
 }
+//导航左按钮我的点击
+-(void)MyselfClick{
+    
+}
+-(void)MessageClick{
+    
+}
+-(void)setUI{
+    self.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithTarget:self action:@selector(MyselfClick) image:@"people" title:@"" EdgeInsets:UIEdgeInsetsMake(0, -10, 0, 0)];
+     self.navigationItem.rightBarButtonItem = [UIBarButtonItem itemWithTarget:self action:@selector(MessageClick) image:@"index-dope" title:@"" EdgeInsets:UIEdgeInsetsMake(0, 0, 10, 0)];
+    self.navigationItem.leftBarButtonItem.badgeValue=self.navigationItem.rightBarButtonItem.badgeValue = @" ";
+    self.navigationItem.leftBarButtonItem.badgeOriginX = 19;
+    self.navigationItem.rightBarButtonItem.badgeOriginX = 29; self.navigationItem.leftBarButtonItem.badgeOriginY = 5;
+    self.navigationItem.rightBarButtonItem.badgeOriginY = -1;
+   
+    
+
+}
+//首页三个按钮点击选中方法
+- (IBAction)btn_selected:(UIButton *)sender {
+    if (sender.tag!=1) {
+        self.btn_findPeople.selected = NO;
+    }
+    if (_tmpBtn == nil){
+        sender.selected = YES;
+        _tmpBtn = sender;
+    }
+    else  if (_tmpBtn !=nil &&_tmpBtn == sender){
+        sender.selected = YES;
+    } else if (_tmpBtn!= sender && _tmpBtn!=nil){
+        _tmpBtn.selected = NO;
+        sender.selected = YES;
+        _tmpBtn = sender;
+    }
+    [UIView animateWithDuration:0.5 animations:^{
+        self.view_line.x = sender.x-20;
+    }];
+//    _selectIndex = sender.tag;
+}
+
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
     [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleDefault;
