4d90646bd4fc2c5a58865388f1b4405c8e133c36
YanShuang Jiang <1048811572@qq.com>
2018-05-20 19:05:17 +0800

    饿得慌

--- ConnectionCity/ConnectionCity/Classes/FindAbilityTab/MakeMoney/MakeMoneyController.m
+++ ConnectionCity/ConnectionCity/Classes/FindAbilityTab/MakeMoney/MakeMoneyController.m
@@ -15,7 +15,8 @@
 #import "WinningBidderController.h"
 #import "CustomPage.h"
 
-@interface MakeMoneyController ()
+@interface MakeMoneyController ()<CustomPageDelegate>
+@property (nonatomic, strong) NSArray *titleArr;
 
 @end
 
@@ -29,10 +30,11 @@
     BountyHostingController *foundVC = [[BountyHostingController alloc]init];
     FootprintController *profileVC = [[FootprintController alloc]init];
     NSArray *arrVC = @[messageVC,addressVC,lifeVC,foundVC,profileVC];
-    NSArray *titleArr = @[@"投标人",@"中标人",@"保证金",@"资金托管",@"足迹"];
+    _titleArr = @[@"投标人",@"中标人",@"保证金",@"资金托管",@"足迹"];
     NSArray *picArr = @[@"Tendering5-5",@"Tendering4-4",@"Tendering3-3",@"Tendering2-2",@"Tendering1-1"];
     NSArray *picSelectArr = @[@"Tendering5",@"Tendering4",@"Tendering3",@"Tendering2",@"Tendering1"];
-    CustomPage *custom = [[CustomPage alloc]initSegmentWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenHeight - kSafeAreaBottomHeight - 64) titlesArray:titleArr withSelectArr:picSelectArr withDeSlectArr:picArr vcOrviews:arrVC];
+    CustomPage *custom = [[CustomPage alloc]initSegmentWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenHeight - kSafeAreaBottomHeight - 64) titlesArray:_titleArr withSelectArr:picSelectArr withDeSlectArr:picArr vcOrviews:arrVC];
+    custom.delegate = self;
     [self.view addSubview:custom];
     
     // Do any additional setup after loading the view from its nib.
@@ -40,9 +42,8 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     self.tabBarController.tabBar.hidden = YES;
-    
-    //    BaseMakeMoneyTabController *tbVC = [[BaseMakeMoneyTabController alloc]init];
-    //    kWindow.rootViewController = tbVC;
+//    BaseMakeMoneyTabController *tbVC = [[BaseMakeMoneyTabController alloc]init];
+//    kWindow.rootViewController = tbVC;
 }
 - (void)viewWillDisappear:(BOOL)animated {
     [super viewDidDisappear:animated];
@@ -51,6 +52,10 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+#pragma mark - CustomPageDelegate
+- (void)selectedIndex:(NSInteger)index {
+    self.navigationItem.title = _titleArr[index];
 }
 
 /*
