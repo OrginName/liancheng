7bb889f55cf37634ac5d70add2ef5c3bed7dfbb7
孤独的流星 <1173918654@qq.com>
2018-05-20 18:10:40 +0800

    UI

---
+++ ConnectionCity/ConnectionCity/Classes/ChangePlay/ChangePlayController.h
@@ -0,0 +1,13 @@
+//
+//  ChangePlayController.h
+//  ConnectionCity
+//
+//  Created by qt on 2018/5/20.
+//  Copyright © 2018年 ConnectionCity. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ChangePlayController : BaseViewController
+
+@end
---
+++ ConnectionCity/ConnectionCity/Classes/ChangePlay/View/ChangeHeadView.m
@@ -0,0 +1,21 @@
+    //
+//  ChangeHeadView.m
+//  ConnectionCity
+//
+//  Created by qt on 2018/5/20.
+//  Copyright © 2018年 ConnectionCity. All rights reserved.
+//
+
+#import "ChangeHeadView.h"
+
+@implementation ChangeHeadView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
