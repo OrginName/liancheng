8b61d5b5dc32f64ec8473f26834e97f00ac8173b
孤独的流星 <1173918654@qq.com>
2018-06-01 01:11:53 +0800

    朋友圈第一版

--- ConnectionCity/ConnectionCity/Category/CustomButton.m
+++ ConnectionCity/ConnectionCity/Category/CustomButton.m
@@ -14,16 +14,32 @@
         self.layer.cornerRadius = 5;
         self.layer.masksToBounds = YES;
         self.titleLabel.font = [UIFont systemFontOfSize:14];
+        self.selectBackColor = [UIColor hexColorWithString:@"#f49930"];
+        self.NOselectBackColor = [UIColor hexColorWithString:@"#f2f2f2"];
+        self.NoSelectTitleColor = [UIColor hexColorWithString:@"#282828"];
+        self.selectTitleColor = [UIColor whiteColor];
     }
     return self;
 }
+-(void)setSelectBackColor:(UIColor *)selectBackColor{
+    _selectBackColor = selectBackColor;
+}
+-(void)setNOselectBackColor:(UIColor *)NOselectBackColor{
+    _NOselectBackColor = NOselectBackColor;
+}
+-(void)setSelectTitleColor:(UIColor *)selectTitleColor{
+    _selectTitleColor = selectTitleColor;
+}
+-(void)setNoSelectTitleColor:(UIColor *)NoSelectTitleColor{
+    _NoSelectTitleColor = NoSelectTitleColor;
+}
 -(void)setSelected:(BOOL)selected{
     if (selected) {
-        [self setBackgroundColor:[UIColor hexColorWithString:@"#f49930"]];
-        [self setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [self setBackgroundColor:self.selectBackColor];
+        [self setTitleColor: self.selectTitleColor forState:UIControlStateNormal];
     }else{
-        [self setBackgroundColor:[UIColor hexColorWithString:@"#f2f2f2"]];
-        [self setTitleColor:[UIColor hexColorWithString:@"#282828"] forState:UIControlStateNormal];
+        [self setBackgroundColor:self.NOselectBackColor];
+        [self setTitleColor:self.NoSelectTitleColor forState:UIControlStateNormal];
     }
 }
 
---
+++ ConnectionCity/ConnectionCity/Classes/FindService/Circle/Cell/CircleCell.h
@@ -0,0 +1,13 @@
+//
+//  CircleCell.h
+//  ConnectionCity
+//
+//  Created by qt on 2018/5/31.
+//  Copyright © 2018年 ConnectionCity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CircleCell : UITableViewCell
+
+@end
