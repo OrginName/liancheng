50f2a26bca4919b5ee282ac6fdb8fdd3344fc0a2
Nsobject11 <1173918654@qq.com>
2018-06-01 10:02:47 +0800

    1

---
+++ ConnectionCity/ConnectionCity/Classes/FindService/Circle/View/FiendPicTxt/FriendCirleTab.h
@@ -0,0 +1,13 @@
+//
+//  FriendCirleTab.h
+//  ConnectionCity
+//
+//  Created by qt on 2018/5/31.
+//  Copyright © 2018年 ConnectionCity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendCirleTab : UITableView
+-(instancetype)initWithFrame:(CGRect)frame withControll:(UIViewController *)control;
+@end
