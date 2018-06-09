f9aee8bc43fa7c505c64f26528924f39c0d8350d
YanShuang Jiang <1048811572@qq.com>
2018-06-09 10:53:23 +0800

    接口

--- ConnectionCity/ConnectionCity/Headers/YSInterface.h
+++ ConnectionCity/ConnectionCity/Headers/YSInterface.h
@@ -47,23 +47,107 @@ static NSString * const v1TalentConnectionCityPage = @"/v1/talent/connection/cit
 static NSString * const v1TalentConnectionEducationPage = @"/v1/talent/connection/education/page";
 //人脉-同行好友列表（分页）
 static NSString * const v1TalentConnectionOccupationPage = @"/v1/talent/connection/occupation/page";
-
-
 #pragma mark - 人才-简历
-
+//筛选条件
+static NSString * const v1TalentResumeConditions = @"/v1/talent/resume/conditions";
+//新增简历
+static NSString * const v1TalentResumeCreate = @"/v1/talent/resume/create";
+//新增简历-教育经历
+static NSString * const v1TalentResumeEducationCreate = @"/v1/talent/resume/education/create";
+//修改简历-教育经历
+static NSString * const v1TalentResumeEducationUpdate = @"/v1/talent/resume/education/update";
+//简历详情
+static NSString * const v1TalentResumeInfo = @"/v1/talent/resume/info";
+//修改简历
+static NSString * const v1TalentResumeUpdate = @"/v1/talent/resume/update";
+//新增简历-工作经历
+static NSString * const v1TalentResumeWorkCreate = @"/v1/talent/resume/work/create";
+//修改简历-工作经历
+static NSString * const v1TalentResumeWorkUpdate = @"/v1/talent/resume/work/update";
 #pragma mark - 人才-赚外快
-
+//评论
+static NSString * const v1TalentTenderCommentCreate = @"/v1/talent/tender/comment/create";
+//招标发布
+static NSString * const v1TalentTenderCreate = @"/v1/talent/tender/create";
+//赚外快-保证金
+static NSString * const v1TalentTenderDepositList = @"/v1/talent/tender/deposit/list";
+//招标详情
+static NSString * const v1TalentTenderDetail = @"/v1/talent/tender/detail";
+//关注招标
+static NSString * const v1TalentTenderFollowCreate = @"/v1/talent/tender/follow/create";
+//赚外快-投标
+static NSString * const v1TalentTenderRecordCreate = @"/v1/talent/tender/record/create";
+//赚外快-投标人
+static NSString * const v1TalentTenderRecordList = @"/v1/talent/tender/record/list";
+//赚外快-中标人
+static NSString * const v1TalentTenderWinRecordList = @"/v1/talent/tender/win-record/list";
 #pragma mark - 关键字信息
-
+//换着玩关键字
+static NSString * const keywordPlayKeyword = @"/keyword/play-keyword";
+//找服务关键字
+static NSString * const keywordServiceKeyword = @"/keyword/service-keyword";
+//找人才关键字
+static NSString * const keywordTalentKeyword = @"/keyword/talent-keyword";
 #pragma mark - 数据字典
-
+//区域树状列表
+static NSString * const dictionaryAreaTreeList = @"/dictionary/area/tree-list";
+//学历
+static NSString * const dictionaryEducation = @"/dictionary/education";
+//职业分类
+static NSString * const dictionaryOccupationCategory = @"/dictionary/occupation-category";
+//专业
+static NSString * const dictionaryProfessional = @"/dictionary/professional";
+//薪资
+static NSString * const dictionarySalary = @"/dictionary/salary";
+//学校
+static NSString * const dictionarySchool = @"/dictionary/school";
+//服务分类
+static NSString * const dictionaryServiceCategory = @"/dictionary/service-category";
+//宝物分类
+static NSString * const dictionaryTreasureCategory = @"/dictionary/treasure-category";
 #pragma mark - 服务
-
+//添加浏览次数
+static NSString * const v1ServiceServiceIdAddBrowseTimes = @"/v1/service/{serviceId}/add-browse-times";
+//筛选条件
+static NSString * const v1ServiceConditions = @"/v1/service/conditions";
+//新增服务
+static NSString * const v1ServiceCreate = @"/v1/service/create";
+//服务详情
+static NSString * const v1ServiceDetail = @"/v1/service/detail";
+//服务列表
+static NSString * const v1ServiceList = @"/v1/service/list";
 #pragma mark - 服务-旅游
-
+//服务-发布陪游
+static NSString * const v1ServiceTravelCreate = @"/v1/service/travel/create";
+//服务-陪游详情
+static NSString * const v1ServiceTravelInfo = @"/v1/service/travel/info";
+//服务-发布邀约
+static NSString * const v1ServiceTravelInviteCreate = @"/v1/service/travel/invite/create";
+//服务-邀约详情
+static NSString * const v1ServiceTravelInviteInfo = @"/v1/service/travel/invite/info";
+//服务-邀约列表（分页）
+static NSString * const v1ServiceTravelInvitePage = @"/v1/service/travel/invite/page";
+//服务-陪游约单
+static NSString * const v1ServiceTravelOrderCreate = @"/v1/service/travel/order/create";
+//服务-旅游列表（分页）
+static NSString * const v1ServiceTravelPage = @"/v1/service/travel/page";
 #pragma mark - 服务-服务站
-
-
+//创建服务站
+static NSString * const v1ServiceStationCreate = @"/v1/service/station/create";
+//服务站详情
+static NSString * const v1ServiceStationInfo = @"/v1/service/station/info";
+//我加入的站点列表
+static NSString * const v1ServiceStationJoinList = @"/v1/service/station/join/list";
+//我的站点列表
+static NSString * const v1ServiceStationMyList = @"/v1/service/station/my/list";
+//附近的服务站
+static NSString * const v1ServiceStationNearbyList = @"/v1/service/station/nearby/list";
+//获取附近，我的，我加入的站点
+static NSString * const v1ServiceStationRelationToMeList = @"/v1/service/station/relation-to-me/list";
+//加入服务站
+static NSString * const v1ServiceStationSign = @"/v1/service/station/sign";
+//退出服务站
+static NSString * const v1ServiceStationSignOut = @"/v1/service/station/sign-out";
 
 
 
