8bbca416a9544c9acb821eb30f4e3f7ea905918a
Nsobject11 <1173918654@qq.com>
2018-07-27 17:28:12 +0800

    myTab

---
+++ ConnectionCity/ConnectionCity/Utils/MyTab/MyTab.h
@@ -0,0 +1,13 @@
+//
+//  MyTab.h
+//  emTab
+//
+//  Created by qt on 2018/7/26.
+//  Copyright © 2018年 qt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <UIScrollView+EmptyDataSet.h>
+@interface MyTab : UITableView<DZNEmptyDataSetSource, DZNEmptyDataSetDelegate>
+
+@end
---
+++ ConnectionCity/ConnectionCity/Utils/MyTab/MyTab.m
@@ -0,0 +1,43 @@
+//
+//  MyTab.m
+//  emTab
+//
+//  Created by qt on 2018/7/26.
+//  Copyright © 2018年 qt. All rights reserved.
+//
+
+#import "MyTab.h"
+@implementation MyTab
+-(void)awakeFromNib{
+    [super awakeFromNib];
+    [self Mydelegate];
+}
+- (instancetype)initWithFrame:(CGRect)frame style:(UITableViewStyle)style{
+    if (self = [super initWithFrame:frame style:style]) {
+        [self Mydelegate];
+    }
+    return self;
+}
+-(void)Mydelegate{
+    self.emptyDataSetSource = self;
+    self.emptyDataSetDelegate = self;
+}
+- (UIImage *)imageForEmptyDataSet:(UIScrollView *)scrollView {
+    return [UIImage imageNamed:@"empty"];
+}
+- (NSAttributedString *)titleForEmptyDataSet:(UIScrollView *)scrollView {
+    NSString *title = @"暂无数据";
+    NSDictionary *attributes = @{
+                                 NSFontAttributeName:[UIFont boldSystemFontOfSize:16.0f],
+                                 NSForegroundColorAttributeName:[UIColor lightGrayColor]
+                                 };
+    return [[NSAttributedString alloc] initWithString:title attributes:attributes];
+}
+- (BOOL)emptyDataSetShouldAllowScroll:(UIScrollView *)scrollView{
+    return YES;
+}
+- (BOOL) emptyDataSetShouldAllowImageViewAnimate:(UIScrollView *)scrollView
+{
+    return YES;
+}
+@end
